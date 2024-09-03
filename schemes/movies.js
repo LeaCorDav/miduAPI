613de74da72ce1156e39680179cd08c2652a4866
Leandro <leaa_67@hotmail.com>
2024-09-03 08:51:48 -0600

    api

---
+++ schemes/movies.js
@@ -0,0 +1,32 @@
+const z = require("zod")
+
+// Esquema de como se debe ingresar una nueva movie
+const movieScheme = z.object({
+    title: z.string({
+        invalid_type_error: "La pelicula debe ser string",
+        required_error: "El titulo es requerido"
+    }),
+    year: z.number().int().min(1900).max(2024),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    poster: z.string().url().endsWith(".jpg" || ".png" || ".jpeg"),
+    genre: z.array(
+        z.enum(["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Thriller", "Sci-Fi", "Anime"])
+    ),
+    rate: z.number().min(0).max(10)
+}) 
+
+// Valida todos los inputs
+function validateMovie(input) {
+    return movieScheme.safeParse(input)
+}
+
+// Valida los inputs que se necesiten
+function validatePartialMovie(input) {
+    return movieScheme.partial().safeParse(input)
+}
+
+module.exports = {
+    validateMovie,
+    validatePartialMovie
+}
